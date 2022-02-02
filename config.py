f92acfbf011269167fcdc3f82c1f38cf5e4a4d3f
Артур Данилов <artur@MacBook-Pro-Artur.local>
2022-02-02 23:45:09 +0300

    Add Procfile

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TOKEN = ""
-QIWI_TOKEN = ""
+TOKEN = "5011732947:AAE9gtK1Lc7sWnGvoSZpVGF-9uQJiBJ0kCQ"
+QIWI_TOKEN = "eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6Im1rMm8yeC0wMCIsInVzZXJfaWQiOiI3OTg1ODc5ODEwOSIsInNlY3JldCI6IjgzMmE5ZjU2MTlmMzdjMDgxNTg5N2JjYmYyMDUwMjQ4ZjUwMjcxMWJmMTg0NjBjNzAxYjJjMGVkMzk0ZDFjNGMifX0="
 
 COST = 5  # Цена подписки в рублях
 TIME = 10  # Срок в днях, на который пользователь продлевает подписку
